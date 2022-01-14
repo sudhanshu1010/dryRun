a3290bfc52a010dea44c9e2ad471b3cabfbb76f5
Sudhanshu Kumar <sudhanshukumar1010@gmail.com>
2022-01-14 18:08:07 +0530

    update

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,53 +2,41 @@ import React, { useState } from 'react';
 import Profile from '../Profile/Profile'
 import './Login.css'
 
-const URL = 'http://127.0.0.1:8000/accounts/login'
+const URLlogin = 'http://127.0.0.1:8000/accounts/login'
 
-const Login = ( {childToParent}) => {
+const Login = ({ childToParent }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [token, setToken] = useState('')
 
-    async function handleInput(){
-        if(!email || !password) alert("Please enter credentials")
+    function handleInput() {
+        if (!email || !password) alert("Please enter credentials")
         const requestOptions = {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json'},
-            body: JSON.stringify({email: email, password: password})
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: email, password: password })
         }
-        
-        const res = await fetch(URL, requestOptions)
-        .then((response) => response.json())
-        .then(json => {
-            // setToken(json.token)
-            // if(token){
-            //     console.log('token no: ', token)
-            // } else {
-            //     console.log('no token found')
-            // }
 
-           setTimeout(
-               function(){
-                   setToken(json.token)
-                   console.log('your token is', token)
-               }, 200
-           )
-                
-        })
-        .catch((error) => console.log(error))
+        const res = fetch(URLlogin, requestOptions)
+            .then((response) => response.json())
+            .then(json => {
+                setToken(json.token)
+            })
+
+            .catch((error) => console.log(error))
     }
 
     return !token ? (
         <div className="login-form-container">
             <div className="login-form">
                 {/* <small className="login-warning">{token ? '' : 'Wrong password!'}</small> */}
-                <input className="form-input email" placeholder="Your email?" type="email" autoComplete="on" onChange={e => setEmail(e.target.value)}/>
-                <input className="form-input password" placeholder="Password" type="password" onChange={e => setPassword(e.target.value)}/>
+                <input className="form-input email" placeholder="Your email?" type="email" autoComplete="on" onChange={e => setEmail(e.target.value)} />
+                <input className="form-input password" placeholder="Password" type="password" onChange={e => setPassword(e.target.value)} />
                 <button className="btn" onClick={() => handleInput()} disabled={!email || !password}>Login</button>
                 <h5>New to DryRun? <a href="#" onClick={() => childToParent(false)}>Create Account</a></h5>
             </div>
         </div>
-    ) : <Profile/>
+    ) : <Profile token={token} />
 
     // return <Profile/>
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Profile.css'
 
-const Profile = () => {
+const URL = 'http://127.0.0.1:8000/accounts/profile'
+
+const Profile = ({ token }) => {
+    const [myData, setMyData] = useState('')
+
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Authorization': 'Token ' + token
+        }
+    }
+
+    const res = fetch(URL, requestOptions)
+        .then((response) => response.json())
+        .then(json => {
+            // setMyData(json)
+            // for(var i in json){
+            //     console.log(json[i])
+            // }
+            console.log(json)
+        })
+        .catch((error) => console.log(error))
+
     return (
         <div className="profile-container">
-            Profile Page
+            Details {myData}
         </div>
     )
 }
