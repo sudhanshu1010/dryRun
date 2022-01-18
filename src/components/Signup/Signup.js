bb6935c873ac53002725c0dab10777f8ffddd0ab
Sudhanshu Kumar <sudhanshukumar1010@gmail.com>
2022-01-18 11:25:47 +0530

    update

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -6,7 +6,12 @@ const Signup = ({ childToParent }) => {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+
     const [confirmPass, checkConfirmPass] = useState(false)
+    const [emailError, setEmailError] = useState(false)
+    const [passwordError, setPasswordError] = useState(false)
+    const [userNameError, setUserNameError] = useState(false)
+
 
     const validateEmail = (email) => {
         const isValid = String(email)
@@ -32,12 +37,28 @@ const Signup = ({ childToParent }) => {
         <div className="signup-container">
             <div className="signup-form">
                 <h2>Sign up</h2>
-                <input className="signup-input email" type="email" placeholder="Email" required onChange={e => validateEmail(e.target.value)} />
+                {/* <input className="signup-input email" type="email" placeholder="Email" required onChange={e => validateEmail(e.target.value)} />
                 <input className="signup-input username" type="text" placeholder="Username" required onChange={e => setUsername(e.target.value)} />
                 <input className="signup-input password" type="password" placeholder="Password" required onChange={e => setPassword(e.target.value)} />
                 <input className="signup-input confirm-password" type="password" placeholder="Confirm Password" required onChange={e => checkConfirmPass(e.target.value)} />
                 <button className="btn btn-primary" onClick={handleInput}>Register</button>
-                <h5>Already have an account? <a href="#" onClick={() => childToParent(true)}>Login</a></h5>
+                <h5>Already have an account? <a href="#" onClick={() => childToParent(true)}>Login</a></h5> */}
+
+                <div className="email-container">
+                    <input type="text" className="form-input email" placeholder="Your email" onChange={e => validateEmail(e.target.value)} />
+                    <small className="email-error error">{emailError ? 'Enter valid email address!' : ''}</small>
+                </div>
+
+                <div className="username-container">
+                    <input type="text" className=""/>
+                    <small className="username-error error">{userNameError ? 'Username already taken' : ''}</small>
+                </div>
+
+                <div className="password-container">
+                    <input className="form-input password" type={showPassword ? 'text' : 'password'} placeholder="Password" onChange={e => setPassword(e.target.value)} />
+                    <span onClick={togglePassword}>{showPassword ? 'Hide' : 'Show'}</span>
+                    <small className="password-error error">{passwordError ? 'Wrong password' : ''}</small>
+                </div>
 
             </div>
         </div>
