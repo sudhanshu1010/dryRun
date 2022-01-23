0d2a553f2b9edf6ea2eb461868560f27edfe73a8
Sudhanshu Kumar <sudhanshukumar1010@gmail.com>
2022-01-23 21:10:20 +0530

    update

--- src/components/Dashboard/Submissions.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Submissions = () => {
-    return (
-        <div>Already</div>
-    )
-}
-
-export default Submissions;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,41 +1,10 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import './Profile.css'
+import React from 'react'
+import styles from './Profile.module.css'
 
-const URL = 'http://127.0.0.1:8000/accounts/profile'
-
-const Profile = ({ token }) => {
-    const [data, setData] = useState(null)
-
-    const requestOptions = {
-        method: 'GET',
-        headers: {
-            // 'Authorization': 'Token 52aee1d3b4e2c2a1041b59a9fdd38140da4b5583'       
-            'Authorization': 'Token ' + token
-        }
-    }
-
-    // const res = (fetch(URL, requestOptions)
-    //     .then((response) => response.json())
-    //     .then(json => {
-    //         // setMyData(json)
-    //         // for(var i in json){
-    //         //     console.log(json[i])
-    //         // }
-    //         data = json;
-    //         console.log(data.name)
-    //         console.log('received data is', json)
-    //     })
-    //     .catch((error) => console.log(error)))
-
-    const res = axios.get(URL, requestOptions).then((response) => response.json()).then(json => console.log(json))
-    // console.log(res.json())
-        
-
+const Profile = () => {
     return (
-        <div className="profile-container">
-            {/* <h6>Email: {data.email}</h6> */}
-            profile page
+        <div className={styles.profile_main_container}>
+            Profile page
         </div>
     )
 }
