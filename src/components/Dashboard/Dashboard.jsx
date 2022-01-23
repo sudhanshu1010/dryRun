8fc337b26f59d8ba51eba6011935a995889dd9fb
Sudhanshu Kumar <sudhanshukumar1010@gmail.com>
2022-01-23 22:25:25 +0530

    update

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,9 +3,23 @@ import styles from './Dashboard.module.css'
 import Navbar from './Navbar'
 
 const Dashboard = () => {
+    const [problem, setProblem] = useState(false)
+    const [explore, setExplore] = useState(false)
+    const [profile, setProfile] = useState(false)
+
+    const getProperties = ({problem, explore, profile }) => {
+        setProblem(problem)
+        setExplore(explore)
+        setProfile(profile)
+    }
+
+    function myFunction(){
+        console.log(problem, explore, profile)
+    }
     return (
         <div className={styles.dashboard_container}>
-            <Navbar/>
+            <Navbar getProperties={problem, explore, profile}/>
+            <button onClick={myFunction}>Click here</button>
         </div>
     )
 }
