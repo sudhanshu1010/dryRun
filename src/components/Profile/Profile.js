62352a3136631f5cebecc112957161d3e25601b9
Sudhanshu Kumar <sudhanshukumar1010@gmail.com>
2022-01-24 12:36:08 +0530

    update

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import styles from './Profile.module.css'
+import Userprofile from '../User/Userprofile'
 
 const Profile = () => {
     return (
         <div className={styles.profile_main_container}>
-            Profile page
+            <Userprofile/>
         </div>
     )
 }
---
+++ src/components/User/Userprofile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Userprofile.module.css'
+
+const Userprofile = () => {
+    return (
+        <div className={styles.userprofile_main_container}>
+            hi
+        </div>
+    )
+}
+
+export default Userprofile;
